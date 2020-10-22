5a4cfb4d320c68e73aac6de4fb655ade36352944
Yuriy Putsan <putsan57@gmail.com>
2020-10-22 21:25:45 +0300

    Updates

---
+++ precache-manifest.8fc6bda5784949fb10299d85051be2e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce59d2432e9d951a1e333962e91d09a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68cad4488abb1c8a8b65",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "636f7771e658e7144128",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "68cad4488abb1c8a8b65",
+    "url": "./static/js/2.0d7cf2a8.chunk.js"
+  },
+  {
+    "revision": "eb745704df7294d386459c0622d6566d",
+    "url": "./static/js/2.0d7cf2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636f7771e658e7144128",
+    "url": "./static/js/main.dfd8a5a8.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.8fc6bda5784949fb10299d85051be2e3.js"
 );
 
 self.addEventListener('message', (event) => {
